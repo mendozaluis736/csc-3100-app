34e7a90191184c975349ccc7e9f70d3017f7e241
mendozaluis736 <lmendo60@calpoly.edu>
2026-08-28 10:39:24 -0700

    IE1 - Added Delete button

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -10,33 +10,32 @@ function TableHeader() {
             </thead>
     );
 }
-function TableBody(){
-    return(
-            <tbody>
-                <tr>
-                    <td>Charlie</td>
-                    <td>Janitor</td>
-                </tr>
-                <tr>
-                    <td>Mac</td>
-                    <td>Bouncer</td>
-                </tr>
-                <tr>
-                    <td>Dee</td>
-                    <td>Aspiring actress</td>
-                </tr>
-                <tr>
-                    <td>Dennis</td>
-                    <td>Bartender</td>
-                </tr>
-            </tbody>
+function TableBody(props) {
+    const rows = props.characterData.map((row, index) => {
+      return (
+        <tr key={index}>
+          <td>{row.name}</td>
+          <td>{row.job}</td>
+          <td>
+            <button onClick ={()=>props.removeCharacter(index)}>Delete</button>
+          </td>
+        </tr>
+      );
+     }
     );
-}
-function Table(){
+    return (
+        <tbody>
+          {rows}
+         </tbody>
+     );
+  }
+function Table(props){
     return (
     <table>
         <TableHeader />
-        <TableBody />
+        <TableBody
+            characterData ={props.characterData}
+            removeCharacter = {props.removeCharacter}/>
     </table>
     );
 }
