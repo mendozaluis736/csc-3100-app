c4947e12a65578ff65f915a208033cf3c6e2fb44
mendozaluis736 <lmendo60@calpoly.edu>
2026-09-13 22:35:24 -0700

    IE3 - added frontent backend communication

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,6 @@
 // backend.js
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -32,7 +33,7 @@ const users = {
       },
     ],
   };
-
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
