504cf66e4542504075510e30511a7d5243688a8e
mendozaluis736 <lmendo60@calpoly.edu>
2026-09-16 18:51:54 -0700

    IE3: added most if not all the self-implementation code for part 4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -5,65 +5,66 @@ import cors from "cors";
 const app = express();
 const port = 8000;
 const users = {
-    users_list: [
-      {
-        id: "xyz789",
-        name: "Charlie",
-        job: "Janitor",
-      },
-      {
-        id: "abc123",
-        name: "Mac",
-        job: "Bouncer",
-      },
-      {
-        id: "ppp222",
-        name: "Mac",
-        job: "Professor",
-      },
-      {
-        id: "yat999",
-        name: "Dee",
-        job: "Aspring actress",
-      },
-      {
-        id: "zap555",
-        name: "Dennis",
-        job: "Bartender",
-      },
-    ],
-  };
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor",
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ],
+};
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send(users);
+  res.send(users);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-const findUserByName = (name)=> {
-  return users["users_list"].filter((user)=>user["name"]===name);
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
 };
 
-const findUserByNameAndJob = (name,job)=>{
-  return users["users_list"].filter((user)=> user["name"] ===name && user["job"]===job);
+const findUserByNameAndJob = (name, job) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name && user["job"] === job
+  );
 };
-app.get("/users",(req,res)=>{
+app.get("/users", (req, res) => {
   const name = req.query.name;
   const job = req.query.job;
   let result;
-  if (name!= undefined && job !== undefined){
-      result = findUserByNameAndJob(name,job);
-    } else if (name !== undefined){
-      result = findUserByName(name);
-    }else{
-      return res.send(users);
-    };
-    res.send(result);
+  if (name != undefined && job !== undefined) {
+    result = findUserByNameAndJob(name, job);
+  } else if (name !== undefined) {
+    result = findUserByName(name);
+  } else {
+    return res.send(users);
   }
-);
+  res.send(result);
+});
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
@@ -76,21 +77,35 @@ app.get("/users/:id", (req, res) => {
     res.send(result);
   }
 });
+const idGeneration = ()=>{
+  return Math.random().toString(25).substring(2,); //tostring cause math.random is just a number and the id we have is a string substring since i didn't like that the id started with 0.
+}
 const addUser = (user) => {
+  user["id"] = idGeneration();
   users["users_list"].push(user);
   return user;
 };
 
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
-  addUser(userToAdd);
-  res.send();
+  const newUser = addUser(userToAdd);
+  res.status(201).send(newUser);
 });
-const deleteUser = (id) =>{
-  users["users_list"] = users["users_list"].filter((u)=>u.id !== id);
+const deleteUser = (id) => {
+  const len = users["users_list"].length;
+  users["users_list"] = users["users_list"].filter((u) => u.id !== id);
+  if (len !== users["users_list"].length){
+    return true;
+  }else{
+    return false;
+  }
 };
-app.delete('/users/:id', (req,res)=>{
+app.delete("/users/:id", (req, res) => {
   const userToDelete = req.params.id;
-  deleteUser(userToDelete);
-  res.send(users["users_list"]);
+  const del = deleteUser(userToDelete);
+  if (del){
+    res.status(204).send(users["users_list"]);
+  }else{
+    res.status(404).send("resource not found")
+  }
 });
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -3,29 +3,76 @@ import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 function MyApp() {
-    const [characters, setCharacters] = useState([]);
-    function fetchUsers(){
-      const promise = fetch("http://localhost:8000/users");
-      return promise;
-    }
+  const [characters, setCharacters] = useState([]);
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+  function postUser(person) {
+    const promise = fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+    return promise;
+  }
+  function deleteUser(id){
+    const promise = fetch(`http://localhost:8000/users/${id}`, {
+      method: "DELETE",
+    });
+    return promise;
+  }
+  function updateList(person) {
+    postUser(person)
+      .then((response) => {
+        if (response.status === 201) {
+          return response.json();
+        } else {
+          console.error(`failed, status ${response.status}`);
+        }
+      })
+      .then((user)=>{
+        if (user){
+          setCharacters([...characters, user]);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-    function removeOneCharacter(index){
-        const updated = characters.filter((character,i) => {
-            return i !== index;
+  function removeOneCharacter(id) {
+    deleteUser(id).then((response)=>{
+      if (response.status === 204){
+        const updated = characters.filter((character) => {
+          return character.id !== id;
         });
         setCharacters(updated);
-    }
-    useEffect(()=>{
-      fetchUsers().then((res)=>res.json()).then((json)=>setCharacters(json["users_list"])).catch((error)=>{console.log(error);});},[]);
+      } else if (response.status == 404){
+          console.error("resource not found");
+      }else{
+        console.error('unkown error');
+      }
+    }).catch((error) => {
+      console.log(error);
+  });
+}
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    function updateList(person){
-        setCharacters([...characters,person]);
-    }
-    return (
-      <div className="container">
-        <Table characterData={characters} removeCharacter={removeOneCharacter}/>
-        <Form handleSubmit={updateList}/>
-      </div>
-    );
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
+    </div>
+  );
 }
 export default MyApp;
